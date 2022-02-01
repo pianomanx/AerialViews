0740c86e37ec952c167fbd84391967cc3a456a73
Neil Turner <neil@neilturner.com>
2022-02-01 14:54:22 +0000

    Merge conflict fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     id("com.google.firebase.crashlytics")
     id("kotlin-android")
     id("kotlin-kapt")
+    id("org.jmailen.kotlinter")
 }
 
 android {
@@ -17,9 +18,7 @@ android {
         minSdk = 23
         targetSdk = 31
         versionCode = 2 // Will be incremented automatically on release
-        versionName = "1.0"
-
-        resValue("string", "app_name", "@string/app_name_release")
+        versionName = "1.1"
 
         manifestPlaceholders["analyticsCollectionEnabled"] = false
         manifestPlaceholders["crashlyticsCollectionEnabled"] = false
@@ -78,8 +77,7 @@ android {
         }
         create("beta") {
             dimension = "version"
-            applicationIdSuffix = ".beta"
-            versionNameSuffix = "-beta"
+            versionNameSuffix = "-beta1"
         }
         create("googleplay") {
             dimension = "version"
@@ -89,7 +87,7 @@ android {
 
 dependencies {
     // Kotlin
-    val kotlinVersion = project.properties["kotlinVersion"]
+    val kotlinVersion = "1.6.10"
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
 
@@ -97,17 +95,22 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
 
+    // Modern Storage
+    implementation("com.google.modernstorage:modernstorage-permissions:1.0.0-alpha05")
+    implementation("com.google.modernstorage:modernstorage-storage:1.0.0-alpha05")
+
     // Android X
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.leanback:leanback:1.0.0")
     implementation("androidx.leanback:leanback-preference:1.0.0")
-    implementation("androidx.preference:preference-ktx:1.1.1")
+    implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("androidx.activity:activity-ktx:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
 
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:29.0.0"))
     implementation("com.google.firebase:firebase-analytics-ktx:20.0.2")
-    implementation("com.google.firebase:firebase-crashlytics-ktx:18.2.6")
+    implementation("com.google.firebase:firebase-crashlytics-ktx:18.2.7")
     implementation("com.google.firebase:firebase-perf-ktx:20.0.4")
 
     // GSON
@@ -117,9 +120,9 @@ dependencies {
     implementation("com.google.android.exoplayer:exoplayer-core:2.16.1")
 
     // Kotpref
-    implementation("com.chibatching.kotpref:kotpref:2.13.1")
-    implementation("com.chibatching.kotpref:initializer:2.13.1")
-    implementation("com.chibatching.kotpref:enum-support:2.13.1")
+    implementation("com.chibatching.kotpref:kotpref:2.13.2")
+    implementation("com.chibatching.kotpref:initializer:2.13.2")
+    implementation("com.chibatching.kotpref:enum-support:2.13.2")
 
     // SMB
     implementation("com.hierynomus:smbj:0.11.3")
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,16 @@ buildscript {
     repositories {
         mavenCentral()
         google()
+        maven("https://plugins.gradle.org/m2/")
     }
     
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:7.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.gms:google-services:4.3.10")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
-        classpath("com.google.firebase:perf-plugin:1.4.0")
+        classpath("com.google.firebase:perf-plugin:1.4.1")
+        classpath("org.jmailen.gradle:kotlinter-gradle:3.8.0")
     }
 }
 
